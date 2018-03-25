6fa26ba1e0bbdc9e73480d134c237a7cc719d073
MR.X <salimovhayali@gmail.com>
2018-03-25 09:46:06 +0300

    Add project files.

---
+++ Palette/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace Palette
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.delay1 = new System.Windows.Forms.RadioButton();
+            this.delay0 = new System.Windows.Forms.RadioButton();
+            this.delay10 = new System.Windows.Forms.RadioButton();
+            this.delay100 = new System.Windows.Forms.RadioButton();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownHeight = 105;
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            resources.ApplyResources(this.comboBox1, "comboBox1");
+            this.comboBox1.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            ((object)(resources.GetObject("comboBox1.Items"))),
+            ((object)(resources.GetObject("comboBox1.Items1"))),
+            ((object)(resources.GetObject("comboBox1.Items2"))),
+            ((object)(resources.GetObject("comboBox1.Items3"))),
+            ((object)(resources.GetObject("comboBox1.Items4"))),
+            ((object)(resources.GetObject("comboBox1.Items5")))});
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Tag = "";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            // 
+            // delay1
+            // 
+            resources.ApplyResources(this.delay1, "delay1");
+            this.delay1.Name = "delay1";
+            this.delay1.UseVisualStyleBackColor = true;
+            // 
+            // delay0
+            // 
+            resources.ApplyResources(this.delay0, "delay0");
+            this.delay0.Checked = true;
+            this.delay0.Name = "delay0";
+            this.delay0.TabStop = true;
+            this.delay0.UseVisualStyleBackColor = true;
+            this.delay0.CheckedChanged += new System.EventHandler(this.delay0_CheckedChanged);
+            // 
+            // delay10
+            // 
+            resources.ApplyResources(this.delay10, "delay10");
+            this.delay10.Name = "delay10";
+            this.delay10.UseVisualStyleBackColor = true;
+            // 
+            // delay100
+            // 
+            resources.ApplyResources(this.delay100, "delay100");
+            this.delay100.Name = "delay100";
+            this.delay100.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            resources.ApplyResources(this.label4, "label4");
+            this.label4.Name = "label4";
+            // 
+            // label5
+            // 
+            resources.ApplyResources(this.label5, "label5");
+            this.label5.Name = "label5";
+            // 
+            // label1
+            // 
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            this.label1.UseCompatibleTextRendering = true;
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.label2.Name = "label2";
+            this.label2.UseCompatibleTextRendering = true;
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.delay10);
+            this.Controls.Add(this.delay100);
+            this.Controls.Add(this.delay0);
+            this.Controls.Add(this.delay1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        protected System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RadioButton delay1;
+        private System.Windows.Forms.RadioButton delay0;
+        private System.Windows.Forms.RadioButton delay10;
+        private System.Windows.Forms.RadioButton delay100;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Palette/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace Palette
+{
+    public partial class Form1 : Form
+    {
+        private string[] color1, color2;
+        private int delay, c;
+        Stopwatch stopWatch = new Stopwatch();
+        Stopwatch stopWatch1 = new Stopwatch();
+
+        public Form1()
+        {
+            InitializeComponent();
+            SetStyle(ControlStyles.AllPaintingInWmPaint | ControlStyles.OptimizedDoubleBuffer | ControlStyles.UserPaint, true);
+            new System.Windows.Forms.Timer() { Interval = 50, Enabled = true }.Tick += delegate { Invalidate(); };
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.SelectedIndex = 0;
+            label1.Top = 15;
+            label2.Top = 15;
+            label1.Left = 40;
+            label2.Left = 560;
+        }
+
+        private void delay0_CheckedChanged(object sender, EventArgs e)
+        {
+            if (delay0.Checked)
+                delay = 0;
+            else if (delay1.Checked)
+                delay = 1;
+            else if (delay10.Checked)
+                delay = 10;
+            else if (delay100.Checked)
+                delay = 100;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var thread1 = new Thread(() => Insert(color1)) { IsBackground = true };
+            var thread2 = new Thread(() => Shell(color2)) { IsBackground = true };
+            thread1.Start();
+            thread2.Start();
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            Draw(e.Graphics, color1, new Rectangle(label1.Location.X - 10, label1.Location.Y + 25, 500, 500));
+            Draw(e.Graphics, color2, new Rectangle(label2.Location.X - 10, label2.Location.Y + 25, 500, 500));
+        }
+
+        private void Draw(Graphics gr, string[] color, Rectangle rect)
+        {
+            TimeSpan ts = stopWatch.Elapsed;
+            TimeSpan ts1 = stopWatch1.Elapsed;
+            string elapsedTime = String.Format("{0:00}:{1:00}.{2:00}", ts.Minutes, ts.Seconds, ts.Milliseconds / 10);
+            string elapsedTime1 = String.Format("{0:00}:{1:00}.{2:00}", ts1.Minutes, ts1.Seconds, ts1.Milliseconds / 10);
+            int Size = 500 / int.Parse(Math.Sqrt(color.Length).ToString());
+            SolidBrush[] myBrush = new SolidBrush[color.Length];
+
+            gr.ResetTransform();
+            var r = rect;
+            r.Inflate(0, 0);
+            gr.DrawRectangle(Pens.Black, r);
+            gr.TranslateTransform(rect.X, rect.Y);
+
+            c = 0;
+            for (int i = 0; i < int.Parse(Math.Sqrt(color.Length).ToString()); i++)
+            {
+                for (int j = 0; j < int.Parse(Math.Sqrt(color.Length).ToString()); j++)
+                {
+                    myBrush[c] = new SolidBrush(ColorTranslator.FromHtml("#" + color[c]));
+                    gr.FillRectangle(myBrush[c], new Rectangle(j * Size, i * Size, Size, Size));
+                    c++;
+                }
+            }
+            label4.Text = "Затрачено: " + elapsedTime;
+            label5.Text = "Затрачено: " + elapsedTime1;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            stopWatch.Reset();
+            stopWatch1.Reset();
+            label4.Text = label5.Text = "Затрачено: 00:00.00";
+            int Amount = int.Parse(comboBox1.SelectedItem.ToString());
+            color1 = new string[Amount];
+            color2 = new string[Amount];
+            var random = new Random();
+            
+            for (int i = 0; i < Amount; i += 2)
+            {
+                color1[i] = color2[i] = String.Format("FF{0:X4}", random.Next(0x100));
+            }
+            for (int i = 1; i < Amount; i += 2)
+            {
+                color1[i] = color2[i] = String.Format("{0:X2}0000", random.Next(0x100));
+            }
+        }
+
+        void Insert(string[] color)
+        {
+            stopWatch.Start();
+            for (int i = 0; i < color.Length; i++)
+            {
+                string current = color[i];
+                int j = i;
+                while (j > 0 && Convert.ToInt32(current, 16) < Convert.ToInt32(color[j - 1], 16))
+                {
+                    color[j] = color[j - 1];
+                    j--;
+                }
+                color[j] = current;
+                Thread.Sleep(delay);
+            }
+            stopWatch.Stop();
+        }
+
+        void Shell(string[] color)
+        {
+            stopWatch1.Start();
+            int j;
+            int step = color.Length / 2;
+            while (step > 0)
+            {
+                for (int i = 0; i < (color.Length - step); i++)
+                {
+                    j = i;
+                    while ((j >= 0) && (Convert.ToInt32(color[j], 16) > Convert.ToInt32(color[j + step], 16)))
+                    {
+                        string current = color[j];
+                        color[j] = color[j + step];
+                        color[j + step] = current;
+                        j -= step;
+                    }
+                }
+                step = step / 2;
+                Thread.Sleep(delay);
+            }
+            stopWatch1.Stop();
+        }
+    }
+}
